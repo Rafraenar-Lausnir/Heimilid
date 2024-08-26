a10d40bc47cf8840afc972a79d25510f52dc3fe9
Elín Ósk <elinosk@elinosk.com>
2024-08-26 21:57:16 +0000

    Initial Commit

---
+++ Heimilid/HeimilidApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeimilidApp.swift
+//  Heimilid
+//
+//  Created by Elín Ósk on 26.8.2024.
+//
+
+import SwiftUI
+
+@main
+struct HeimilidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
